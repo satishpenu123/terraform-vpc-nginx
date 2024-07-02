3917d6818430962c9becaa9d8b2ceeb6a34bd577
satishpenu123 <penumallisatish1@gmail.com>
2024-07-02 02:07:03 -0700

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -5,16 +5,19 @@ variable "cidrblk" {
   default = "10.0.0.0/16"
 }
 
-variable "pubsubblk" {
+variable "pubsub1blk" {
   type = "string"
   default = "10.0.1.0/24"
 }
 
-variable "prvsubblk1" {
+variable "pubsub2blk" {
   type = "string"
   default = "10.0.2.0/24"
 }
-
+variable "prvsubblk" {
+  type = "string"
+  default = "10.0.3.0/24"
+}
 variable "key_name" {
   type = "string"
   default = "nginx_kp"
