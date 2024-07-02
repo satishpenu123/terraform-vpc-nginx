bd838f939691a52c23e2f0d7cf2621a5b643e112
satishpenu123 <penumallisatish1@gmail.com>
2024-07-02 00:44:01 -0700

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,1 +1,26 @@
 
+#variables start
+variable "cidrblk" {
+  type = "string"
+  default = "10.0.0.0/16"
+}
+
+variable "pubsubblk" {
+  type = "string"
+  default = "10.0.1.0/24"
+}
+
+variable "prvsubblk1" {
+  type = "string"
+  default = "10.0.2.0/24"
+}
+
+variable "key_name" {
+  type = "string"
+  default = "nginx_kp"
+}
+
+variable "ami_id" {
+  type = "string"
+  default = "ami-cd0f5cb6"
+}
